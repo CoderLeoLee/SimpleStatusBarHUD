b1c66082b19148b58c36a74b67813ab288686327
why <why@163.com>
2015-09-22 11:13:16 +0800

    删除了不必要的方法
    请谨慎考虑

--- XMGStatusBarHUDDemo/XMGStatusBarHUDDemo/ViewController.m
+++ XMGStatusBarHUDDemo/XMGStatusBarHUDDemo/ViewController.m
@@ -16,15 +16,7 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+// 删除了不必要的方法
 
 - (IBAction)success {
     [XMGStatusBarHUD showSuccess:@"成功加载"];
